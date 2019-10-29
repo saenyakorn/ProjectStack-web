527220d734b136f449380269a8408547aa59853b
Saenyakorn <saenyakorn@Saenyakorns-MacBook-Pro.local>
2019-10-29 16:41:53 +0700

    Added home page

--- script/index.js
+++ script/index.js
@@ -1,2 +1,20 @@
-console.log($('#project-container').html())
-console.log($('#trending-container').html())
+$('.ui.sidebar').sidebar({
+    context: $('.ui.pushable.segment'),
+    transition: 'overlay'
+}).sidebar('attach events', '#mobile_item');
+
+$('#project-generator').html("");
+$('#trending-generator').html("");
+$('div.ui.pushable.segment').css("height", $(window).height() - $('div.ui.menu').height() - 1);
+
+$.get('./generator/project-card.html', (result) => {
+    for (var i = 0; i < 20; i++) {
+        $('#project-generator').append(result);
+    }
+});
+
+$.get('./generator/trending-card.html', (result) => {
+    for (var i = 0; i < 5; i++) {
+        $('#trending-generator').append(result);
+    }
+});
