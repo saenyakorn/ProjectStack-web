8ff800282ee9f4e0380e3b6b83ae2f4807c82db9
Saenyakorn <saenyakorn@Saenyakorns-MacBook-Pro.local>
2019-10-31 00:44:08 +0700

    Fixed home page

--- script/index.js
+++ script/index.js
@@ -3,6 +3,15 @@ $('.ui.sidebar').sidebar({
     transition: 'overlay'
 }).sidebar('attach events', '#mobile_item');
 
+$('.ui.sidebar').sidebar({
+    context: $('.ui.pushable.segment'),
+    transition: 'overlay'
+}).sidebar('attach events', '#computer_item');
+
+$('.ui.icon.top.left.pointing.dropdown.button').dropdown({
+    clearable: false
+});
+
 $('#project-generator').html("");
 $('#trending-generator').html("");
 $('div.ui.pushable.segment').css("height", $(window).height() - $('div.ui.menu').height() - 1);
