eed40e9e337cc287df0ae4a6d545516e587a816a
IristuN <nutrock007@gmail.com>
2019-10-31 23:15:13 +0700

    newteam-script

--- script/newteam-script.js
+++ script/newteam-script.js
@@ -1,3 +1,15 @@
+//cookie function
+function getcookie() {
+    var x = document.cookie.split(";");
+    var y = {}
+    for (var i = 0; i < x.length; i++) {
+        var z = x[i].split("=");
+        y[z[0]] = z[1];
+    }
+    return y;
+};
+
+
 //yu
 $('.ui.sidebar').sidebar({
     context: $('.ui.pushable.segment'),
@@ -14,19 +26,27 @@ $('.ui.icon.top.left.pointing.dropdown.button').dropdown({
 });
 
 $('div.ui.pushable.segment').css("height", $(window).height() - $('div.ui.menu').height() - 1);
+
+
+
 // anos-script
 $('.ui.dropdown').dropdown();
 $('.ui.sidebar').sidebar({
     context: $('.ui.pushable.segment'),
     transition: 'overlay'
 }).sidebar('attach events', '#mobile_item');
+
+
+
 //nut-script
 var content = [];
 var obj_username = {};
 $.ajax({
     url: "https://projectstack.no.sh/user/all",
     type: "POST",
-    data: {},
+    data: {
+        fields: ["username", "email", "profilepic_url"]
+    },
     datatype: "json",
     success: function(result) {
         console.log(result);
@@ -34,24 +54,34 @@ $.ajax({
             content.push({ title: user_obj.username });
             obj_username[user_obj.username] = user_obj;
         });
+        $('.ui.search').search({
+            source: content
+        });
+        //leader generator
+        var leader_cookie = getcookie()
+        var nln = leader_cookie.username;
+        var val = { username: nln, email: obj_username[nln].email, p_pic_url: obj_username[nln].profilepic_url };
+        $.get('../generator/member-card-mustache.html', (html) => {
+            var output = Mustache.render(html, val);
+            $('#leader-generator').append(output);
+        });
     },
     error: function(error) {
         console.log(error);
     }
 });
 
-//anos
-$('.ui.search').search({
-    source: content
-});
+
+
 
 //nut
 $("#plus-member").click(function() {
     if ($("#new-member-name").value() in obj_username) {
-        var val = { username: $("#new-member-name").value(), email: obj_username[$("#new-member-name").value()].email };
-        $.get('../generator/member-card-mustache', (html) => {
+        var nmn = $("#new-member-name").value()
+        var val = { username: nmn, email: obj_username[nmn].email, p_pic_url: obj_username[nmn].profilepic_url };
+        $.get('../generator/member-card-mustache.html', (html) => {
             var output = Mustache.render(html, val);
-            $('#project-generator').append(output);
+            $('#member-generator').append(output);
         });
         $("#new-member-name").value() = null;
     } else {
@@ -60,14 +90,16 @@ $("#plus-member").click(function() {
 });
 $("#create").click(function() {
     var valid = true;
-    if ($('#teamname').value() == null) return;
+    if ($('#team-name').value() == null) return;
 
     var members_array = [];
-    $("")
+    $('.member-username').each(function() {
+        members_array.push($(this).value());
+    });
 
     const team_info = {
-        teamName: $('#teamname').value(),
-        leaderID: " ",
+        teamName: $('#team-name').value(),
+        leaderID: $('#leader-name').value(),
         members: members_array
     };
     $.ajax({
@@ -84,4 +116,8 @@ $("#create").click(function() {
         }
 
     });
+});
+
+$(".delete-but").click(function() {
+    $("#member-generator").remove($(this).parentNode.parentNode)
 });
