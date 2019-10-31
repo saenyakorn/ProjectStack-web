e5821bcd28be3e39a6107531cfd58f596f8e5477
Sorawit Thipsingh <56878615+get44754@users.noreply.github.com>
2019-10-31 21:07:05 +0700

    hello u

--- script/edit-profile.js
+++ script/edit-profile.js
@@ -8,6 +8,13 @@ $('.ui.sidebar').sidebar({
     transition: 'overlay'
 }).sidebar('attach events', '#computer_item');
 
+$('.ui.icon.top.left.pointing.dropdown.button').dropdown({
+    clearable: false
+});
+
+$('div.ui.pushable.segment').css("height", $(window).height() - $('div.ui.menu').height() - 1);
+
+
 
 
 $('#edit-submit').click(function() {
@@ -33,4 +40,16 @@ $('#edit-submit').click(function() {
         resume5: allinput[10],
         link: allinput[11],
     };
+    $.ajax({
+        url: "https://projectstack.now.sh/",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(result) {
+            console.log(result);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    })
 })
