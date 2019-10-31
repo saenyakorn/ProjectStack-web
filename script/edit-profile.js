ab119f31b227907b560b565d3ff1ccc5a3bd6450
Sorawit Thipsingh <56878615+get44754@users.noreply.github.com>
2019-11-01 04:32:52 +0700

    u za123

--- script/edit-profile.js
+++ script/edit-profile.js
@@ -14,6 +14,31 @@ $('.ui.icon.top.left.pointing.dropdown.button').dropdown({
 
 $('div.ui.pushable.segment').css("height", $(window).height() - $('div.ui.menu').height() - 1);
 
+function getcookie() {
+    var x = document.cookie.split(";");
+    var y = {}
+    for (var i = 0; i < x.length; i++) {
+        var z = x[i].split("=");
+        y[z[0]] = z[1];
+    }
+    return y;
+};
+
+
+
+
+
+
+const cookie = getcookie();
+$('#username').html(cookie.username)
+if (!("username" in cookie)) {
+    $('.guest').css('display', 'flex');
+    $('.logged-in').css('display', 'none');
+} else {
+    $('.guest').css('display', 'none');
+    $('.logged-in').css('display', 'flex');
+}
+
 
 
 
